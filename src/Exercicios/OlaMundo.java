96474527bc2a63866067426d96ece9406b43e92e
Victor <Victorbicalhoa@outlook.com>
2025-04-16 19:21:57 -0300

    Reentrega - Parte 1

---
+++ src/Exercicios/OlaMundo.java
@@ -0,0 +1,9 @@
+package Exercicios;
+
+public class OlaMundo {
+
+        public static void main(String[] args) {
+            System.out.println("Olá, meu nome é Hebert Victor e estou aprendendo java!");
+        }
+
+}
